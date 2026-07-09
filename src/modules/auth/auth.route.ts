54f3fc31f2c80b8aa9d36c249b5a4cfb6ef3c043
EbnulAhsan <ebnu1721@gmail.com>
2026-07-10 01:04:02 +0600

    new route added for generate new refresh token for user

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -8,4 +8,8 @@ const router = Router();
 
 router.post("/login", authController.loginUser);
 
+//  creating new route for generating new refresh token for user after expired the previous one
+
+router.post("/refresh-token", authController.refreshToken)
+
 export const authRoutes = router;
