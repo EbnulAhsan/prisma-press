7889452cb87b788915402c851dd2a7ba50db20ec
EbnulAhsan <ebnu1721@gmail.com>
2026-07-09 23:43:16 +0600

    auth login route error fixed

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -3,6 +3,9 @@ import { authController } from "./auth.controller";
 
 const router = Router();
 
+
+//  auth login route 
+
 router.post("/login", authController.loginUser);
 
 export const authRoutes = router;
