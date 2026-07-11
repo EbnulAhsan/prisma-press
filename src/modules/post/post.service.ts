791ea925f760cf0644326953e919c86b66210bc5
EbnulAhsan <ebnu1721@gmail.com>
2026-07-11 22:35:29 +0600

    post service stracture done

---
+++ src/modules/post/post.service.ts
@@ -0,0 +1,34 @@
+
+const creatPost = () => {
+
+}
+
+const getAllPosts = () => {
+
+}
+
+const getPostById = () => {
+
+}
+
+
+const updatePost = () => {
+
+}
+
+const deletePost = () => {
+
+}
+
+
+const getPostsStats = () => {
+
+}
+
+const getMyPosts = () => {
+
+}
+
+export const postService = {
+    creatPost, getAllPosts, getPostById, updatePost, deletePost, getPostsStats, getMyPosts
+}
