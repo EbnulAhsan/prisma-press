0d749a4c8dd04d8a34f02cca1bbbdfaa608ec1d8
EbnulAhsan <ebnu1721@gmail.com>
2026-07-11 22:28:11 +0600

    post controller structure done

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -9,6 +9,63 @@ const createPost = catchAsync(async (req: Request, res: Response, next: NextFunc
 })
 
 
+//  get all post
+const getAllPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+
+// get the posy by user id 
+
+const getPostById = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+
+// update the post
+const updatePost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+
+// delete the post
+const deletePost = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+// get the post stats
+
+const getPostsStats = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+// get my post
+
+const getMyPosts = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+
+})
+
+
+export const postController = {
+    createPost, getAllPosts, getPostById, updatePost, deletePost, getPostsStats, getMyPosts
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
