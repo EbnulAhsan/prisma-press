64dcde6559d86865313f854d234111be537f81b0
EbnulAhsan <ebnu1721@gmail.com>
2026-07-13 20:45:39 +0600

    updatepost service done

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,5 +1,5 @@
 import { prisma } from "../../lib/prisma"
-import { ICreatePostPayload } from "./post.interface"
+import { ICreatePostPayload, IUpdatePostPayload } from "./post.interface"
 
 const creatPost = async (payload: ICreatePostPayload, userId: string) => {
 
@@ -70,7 +70,38 @@ const getPostById = async (postId: string) => {
 }
 
 
-const updatePost = (postId: string, payload : IUpdatePostPayload, authorId: string, isAdmin: boolean) => {
+const updatePost = async (postId: string, payload: IUpdatePostPayload, authorId: string, isAdmin: boolean) => {
+
+    const post = await prisma.post.
+        findFirstOrThrow({
+            where: {
+                id: postId
+
+            }
+        })
+
+    if (!isAdmin && post.authorId !== authorId) {
+        throw new Error("you are not the owner of this post ")
+
+    }
+
+    const result = await prisma.post.update({
+        where: {
+            id: postId
+        },
+        data: payload,
+        include: {
+            author: {
+                omit: {
+                    password: true
+                }
+            },
+            comments: true
+        }
+    })
+
+    return result
+
 
 }
 
