004ea9810a46ceb97d9f4ac3acfd69ca52731059
EbnulAhsan <ebnu1721@gmail.com>
2026-07-14 14:43:46 +0600

    fig the error

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -264,25 +264,26 @@ const getPostsStats = async () => {
 
             //     //  now total post view with direct connecting with prisma 
 
-            //     const totalPostViewsAggregate = await tx.post.aggregate({
-            //         _sum: {
-            //             views: true
-            //         }
-            //     })
+            // const totalPostViewsAggregate = await tx.post.aggregate({
+            //     _sum: {
+            //         views: true
+            //     }
+            // })
 
             //     const totalPostViews = totalPostViewsAggregate._sum.views
 
 
-            //     return {
-            //         totalPosts, totalPublishedPosts, totalDraftPosts, totalArchivedPosts, totalComments, totalApprovedComments, totalRejectedComments,totalPostViews
+            // return {
+            //     totalPosts, totalPublishedPosts, totalDraftPosts, totalArchivedPosts, totalComments, totalApprovedComments, totalRejectedComments,totalPostViews
 
-            //     }
+            // }
 
 
             // usuing the best approach
 
 
-            await Promise.all([
+            const [totalPosts, totalPublishedPosts, totalDraftPosts, totalArchivedPosts, totalComments, totalApprovedComments, totalRejectedComments, totalPostViewsAggregate
+            ] = await Promise.all([
                 await tx.post.count(),
                 await tx.post.count({
                     where: {
@@ -320,6 +321,12 @@ const getPostsStats = async () => {
                     where: {
                         status: CommentStatus.REJECT
                     }
+                }),
+
+                await tx.post.aggregate({
+                    _sum: {
+                        views: true
+                    }
                 })
 
 
@@ -327,15 +334,13 @@ const getPostsStats = async () => {
 
 
 
-
-
-
-
-
-
-
-
             ])
+
+            return {
+                totalPosts, totalPublishedPosts, totalDraftPosts, totalArchivedPosts, totalComments, totalApprovedComments, totalRejectedComments, 
+                totalPostViews  : totalPostViewsAggregate._sum.views
+
+            }
 
 
 
