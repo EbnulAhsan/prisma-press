a3b44dff016721112c56023632948f4367a15711
EbnulAhsan <ebnu1721@gmail.com>
2026-07-17 23:01:45 +0600

    working on comment-controller

---
+++ src/modules/comment/comment.controller.ts
@@ -0,0 +1,9 @@
+import { NextFunction } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+
+
+//  create comment 
+const CreateComment = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+    
+    
+})
