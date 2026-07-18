9784181649236fff0c0b26f3524d04fa40c03881
EbnulAhsan <ebnu1721@gmail.com>
2026-07-18 18:22:46 +0600

    new error route build

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import cookieParser from "cookie-parser";
-import express, { Application, Request, Response } from "express";
+import express, { Application,  Request, Response } from "express";
 import cors from "cors";
 import config from "./config";
 import { userRoutes } from "./modules/user/user.route";
 import { authRoutes } from "./modules/auth/auth.route";
 import { postRoutes } from "./modules/post/post.route";
 import { commentRoutes } from "./modules/comment/comment.route";
+import path from "node:path";
+import { notFound } from "./middlewares/notFound";
 
 
 const app: Application = express();
@@ -38,6 +40,18 @@ app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes)
 app.use("/api/comments", commentRoutes) 
 
+
+
+
+// app.use((req: Request, res: Response) => {
+//     res.status(404).json({
+//         message: " Route not found",
+//         path: req.originalUrl,
+//         Date: Date()
+//     })
+// })
+
+app.use(notFound)
 
 
 
