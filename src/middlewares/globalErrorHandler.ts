03e360a00f67423a5bbd08d4f8b4771259c07e0a
EbnulAhsan <ebnu1721@gmail.com>
2026-07-19 02:27:52 +0600

    handeling error

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -1,11 +1,39 @@
 import { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status"
+import { Prisma } from "../../generated/prisma/client";
+import { error } from "node:console";
+
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
     console.log("error: ", err)
 
-    
+    let statusCode
+    let errorMessage = err.message || "Internal Server Error " || "Internal Server Error "
+
+    let errorName = err.name || "Internal Server Error "
+
+
+    if (err instanceof Prisma.PrismaClientValidationError) {
+        statusCode = httpStatus["400_NAME"]
+        const errorMessage = " you have provided incorrect values, please check it again "
+
+    } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+
+        if (err.code === "p2002") {
+
+            statusCode = httpStatus.BAD_REQUEST,
+                errorMessage = "duplicate key error"
+
+        } else if (err.code === "p2003") {
+            statusCode = httpStatus.BAD_REQUEST,
+                errorMessage = " foreign key constraint failed "
+        } else if (err.code === "p2005") {
+            statusCode = httpStatus.BAD_REQUEST,
+                errorMessage = "  an operation failed bcz it depends on one or more records that were required but not found "
+        }
+
+    }
 
 
 
@@ -24,9 +52,10 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
 
     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
         success: false,
-        statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-        message: err.message,
-        error: err
+        statusCode: statusCode || httpStatus.INTERNAL_SERVER_ERROR,
+        message: errorMessage,
+        error: err.stack,
+        name: errorName
     });
 
 }
