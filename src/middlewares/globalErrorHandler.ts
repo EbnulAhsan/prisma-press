b2aa73c443cee73fb34a0de16340e328b8decf65
EbnulAhsan <ebnu1721@gmail.com>
2026-07-19 01:57:07 +0600

    working on global-error-handler

--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -3,6 +3,25 @@ import httpStatus from "http-status"
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
 
+    console.log("error: ", err)
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
         success: false,
         statusCode: httpStatus.INTERNAL_SERVER_ERROR,
