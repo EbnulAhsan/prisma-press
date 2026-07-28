2e6bf7bd75a1db0239ccbe5cd0d46aa9e3faea9d
EbnulAhsan <ebnu1721@gmail.com>
2026-07-28 21:02:19 +0600

    controller done subscription

--- src/modules/subscription/subscription.controller.ts
+++ src/modules/subscription/subscription.controller.ts
@@ -4,6 +4,7 @@ import { subscriptionService } from "./subscription.service";
 import { sendResponse } from "../../utils/senResponse";
 import { ok } from "node:assert";
 import httpStatus from "http-status"
+import { send } from "node:process";
 
 
 const createCheckoutSession = catchAsync(
@@ -31,6 +32,17 @@ const createCheckoutSession = catchAsync(
 
 const handleWebhook = catchAsync(
     async (req: Request, res: Response, next: NextFunction) => {
+        const event = req.body as string
+        
+        const signature = req.headers['stripe-signatuire']!
+
+        sendResponse(res, {
+            success: true,
+            statusCode: 200,
+            message: "webhook triggered successfully",
+            data:null
+        })
+
         
     }
 )
